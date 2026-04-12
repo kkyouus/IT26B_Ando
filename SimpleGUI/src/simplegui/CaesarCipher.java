9fa6b4981a8ba803fddf8277f131d15e5377baf2
kkyouus <andokarljohnreybautista@gmail.com>
2026-04-12 22:58:58 +0800

    added new CaesarCipher

--- SimpleGUI/src/simplegui/CaesarCipher.java
+++ SimpleGUI/src/simplegui/CaesarCipher.java
@@ -1,7 +1,26 @@
-
 package simplegui;
 
-
 public class CaesarCipher {
-    
+
+    private static final int SHIFT = 3;
+
+    public static String encrypt(String text) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : text.toCharArray()) {
+            sb.append((char) (c + SHIFT));
+        }
+
+        return sb.toString();
+    }
+
+    public static String decrypt(String text) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : text.toCharArray()) {
+            sb.append((char) (c - SHIFT));
+        }
+
+        return sb.toString();
+    }
 }

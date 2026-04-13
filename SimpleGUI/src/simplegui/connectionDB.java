596b3f129c41b5f93785a127f185046cbdac11f0
kkyouus <andokarljohnreybautista@gmail.com>
2026-04-13 22:59:21 +0800

    fix connectionDB

--- SimpleGUI/src/simplegui/connectionDB.java
+++ SimpleGUI/src/simplegui/connectionDB.java
@@ -5,24 +5,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class connectionDB {
-    
-    // Database credentials
+
     private static final String URL = "jdbc:mysql://localhost:3306/it26b";
     private static final String USER = "root";
     private static final String PASSWORD = "";
-    
+
     public static Connection getConnection() {
         Connection conn = null;
-        
-        try{
+
+        try {
             conn = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Database Connected Successfully!");
-            
         } catch (SQLException e) {
             System.out.println("Connection Failed!");
             e.printStackTrace();
         }
+
         return conn;
     }
-    
 }

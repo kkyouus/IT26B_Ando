260661ab43c31e8a9b4b0360eebf053a65a17d99
kkyouus <andokarljohnreybautista@gmail.com>
2026-04-13 21:50:21 +0800

    added connection method

--- SimpleGUI/src/simplegui/connectionDB.java
+++ SimpleGUI/src/simplegui/connectionDB.java
@@ -1,7 +1,28 @@
-
 package simplegui;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class connectionDB {
+    
+    // Database credentials
+    private static final String URL = "jdbc:mysql://localhost:3306/it26b";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    
+    public static Connection getConnection() {
+        Connection conn = null;
+        
+        try{
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Database Connected Successfully!");
+            
+        } catch (SQLException e) {
+            System.out.println("Connection Failed!");
+            e.printStackTrace();
+        }
+        return conn;
+    }
     
 }
